1b36d4a63ed63dded69c289707f392b1d1c0ee03
Y <11781448+xz0816@user.noreply.gitee.com>
2022-10-29 02:20:08 +0000

    update info.json-master/UI.js.
    Signed-off-by: Y. <11781448+xz0816@user.noreply.gitee.com>

--- info.json-master/UI.js
+++ info.json-master/UI.js
@@ -105,7 +105,7 @@ ui.layout(
                             <horizontal  gravity="center_vertical" padding="5 5" >
                                 <View bg="#00BFFF" h="*" w="10"  ></View>
                                 <vertical padding="10 8" h="auto" w="0" layout_weight="1">
-                                    <text w="auto" textColor="#222222" textSize="15sp" text="点点通功能" />
+                                    <text w="auto" textColor="#222222" textSize="15sp" text=" " />
                                 </vertical>
                                 <checkbox id="ttxs_pro_ddtong" marginLeft="4" marginRight="6" checked="false" />
                             </horizontal>
